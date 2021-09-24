c37e1055831418b9cb00daf98ca69edb098031bf
guaaan <juampablo.txt@gmail.com>
2021-09-24 15:41:00 -0300

    abrir ventana y enviar parametros a textbox al hacer click

--- imagenProducto.Designer.cs
+++ imagenProducto.Designer.cs
@@ -29,19 +29,63 @@ namespace ptoVenta
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtLinea = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.Color.White;
+            this.txtNombre.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.ForeColor = System.Drawing.Color.DimGray;
+            this.txtNombre.Location = new System.Drawing.Point(12, 12);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(261, 22);
+            this.txtNombre.TabIndex = 283;
+            this.txtNombre.TabStop = false;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.BackColor = System.Drawing.Color.White;
+            this.txtCodigo.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigo.ForeColor = System.Drawing.Color.DimGray;
+            this.txtCodigo.Location = new System.Drawing.Point(12, 40);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(261, 22);
+            this.txtCodigo.TabIndex = 284;
+            this.txtCodigo.TabStop = false;
+            // 
+            // txtLinea
+            // 
+            this.txtLinea.BackColor = System.Drawing.Color.White;
+            this.txtLinea.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLinea.ForeColor = System.Drawing.Color.DimGray;
+            this.txtLinea.Location = new System.Drawing.Point(12, 68);
+            this.txtLinea.Name = "txtLinea";
+            this.txtLinea.Size = new System.Drawing.Size(261, 22);
+            this.txtLinea.TabIndex = 285;
+            this.txtLinea.TabStop = false;
             // 
             // imagenProducto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(444, 350);
+            this.ClientSize = new System.Drawing.Size(285, 198);
+            this.Controls.Add(this.txtLinea);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.txtNombre);
             this.Name = "imagenProducto";
             this.Text = "imagenProducto";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        public System.Windows.Forms.TextBox txtNombre;
+        public System.Windows.Forms.TextBox txtCodigo;
+        public System.Windows.Forms.TextBox txtLinea;
     }
 }
