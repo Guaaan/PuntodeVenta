1c98d0d2bc070d02056d4b7e6d03dab9a8c78263
guaaan <juampablo.txt@gmail.com>
2021-11-17 16:16:53 -0300

    limpieza de código

--- ticketsEmitidos.cs
+++ ticketsEmitidos.cs
@@ -99,57 +99,7 @@ namespace ptoVenta
                     printmir.CargarImprimir(dgvGrid1, printDocumento, vnum);
 
                 }
-                //if (dgvGrid1.CurrentCell.ColumnIndex == 9)
-                //{
-                //    int indice = dgvGrid1.CurrentRow.Index;
-                //    string ddoc = Convert.ToString(dgvGrid1.Rows[indice].Cells[0].Value).Trim();
-                //    printmir.CargarImprimir(printDocumento, ddoc);
-                //}
-                /*if (dgvGrid1.CurrentCell.ColumnIndex == 9)
-                {
-                    //encapsula en vnum el numero de boleta
-                    vnum = dgvGrid1.CurrentRow.Cells["NUMERO"].Value.ToString();
-
-                    //RepTicket rep = new RepTicket();
-                    LocalReport report = new LocalReport();
-                    report.DataSources.Clear();
-
-                    Form1.MiReporte = "Informes\\Tickets.rdlc";
-                    printmir.CargarImprimir(printDocumento, vnum);
-
-                    foreach (DataGridViewRow row in dgvGrid1.Rows)
-                    {
-                        if (row.Cells["numero"].Value.ToString() == vnum)
-                        {
-                            TicketDatos dat = new TicketDatos();
-                            dat.Codigo = row.Cells["codigo"].Value.ToString();
-                            dat.Nombre = row.Cells["producto"].Value.ToString();
-                            dat.Cantidad = row.Cells["cantidad"].Value.ToString();
-                            double pre = (double)row.Cells["precio"].Value;
-                            dat.Precio = pre.ToString("N0");
-                            TicketDatos.Add(dat);
-                        }
-                    }
-                    //rep.ShowDialog();
-                    report.DataSources.Add(new ReportDataSource("DataSet1", TicketDatos));
-                    report.ReportPath = (Form1.MiReporte);
-
-                    string vRif = Form1.erif.Trim();
-                    string vCaja = iniciarSesion.ucodigo.Trim();
-                    string vNro = vnum;
-
-                    ReportParameter[] parameters = new ReportParameter[3];
-                    parameters[0] = new ReportParameter("rRif", vRif);
-                    parameters[1] = new ReportParameter("rCaja", vCaja);
-                    parameters[2] = new ReportParameter("rNro", vNro);
-
-                    report.SetParameters(parameters);
-                    report.PrintToPrinter();
-
-                }*/
-                //finrdlc
-
-                    //comienzo de ascii
+                
                 
                 if (dgvGrid1.CurrentCell.ColumnIndex == 10)
                 {
@@ -158,86 +108,9 @@ namespace ptoVenta
                     abrirFormClass.ShowDialog();
                 }
             }
-            //printDocumento = new PrintDocument();
-            //PrinterSettings ps = new PrinterSettings();
-            //printDocumento.PrinterSettings = ps;
-            //printDocumento.PrintPage += Imprimir;
-            //printDocumento.Print();
+           
         }
-        //private void Imprimir(object sender, PrintPageEventArgs e)
-        //{
-        //    Font header = new Font("Arial", 14);
-        //    Font font = new Font("Arial", 11);
-        //    Font fuente = new Font("Arial", 8);
-        //    int y = 20;
-        //    int ancho = 270;
-
-        //    StringFormat formato1 = new StringFormat(StringFormatFlags.NoClip);
-        //    StringFormat formato2 = new StringFormat(formato1);
-
-        //    formato1.LineAlignment = StringAlignment.Near;
-        //    formato1.Alignment = StringAlignment.Center;
-        //    formato2.LineAlignment = StringAlignment.Center;
-        //    formato2.Alignment = StringAlignment.Far;
-
-
-        //    //Rectangle displayRectangle = new Rectangle(new Point(0, 20), new Size(240, 20));
-
-        //    string LineEncabezado = "Cant  Articulo‎‎‎‎‏‏‎";   // agrega lineas de  encabezados
-
-        //    //header
-        //    e.Graphics.DrawString("FARMACIAS GEMINIS", header, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //    e.Graphics.DrawString("————Punto de Venta————————————————————————", font, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //    e.Graphics.DrawString("Rut: " + Form1.erif, font, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //    e.Graphics.DrawString("Fecha: " + DateTime.Now.ToString(), font, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //    e.Graphics.DrawString("Caja: " + iniciarSesion.ucodigo, font, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //    e.Graphics.DrawString("COTIZACIÓN", font, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //    e.Graphics.DrawString("————Productos—————————————————————————", font, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //    e.Graphics.DrawString(LineEncabezado, font, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //    e.Graphics.DrawString("Valor", font, Brushes.Black, new RectangleF(0, y += -5, ancho, 20), formato2);
-        //    e.Graphics.DrawString("   ", font, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-
-
-
-        //    //----fin del header
-        //    double montoTotal = 0;
-
-        //    foreach (DataGridViewRow row in dgvGrid1.Rows)
-        //    {
-
-        //        TicketDatos dato = new TicketDatos();
-        //        dato.Codigo = row.Cells["codigo"].Value.ToString();
-        //        dato.Nombre = row.Cells["producto"].Value.ToString();
-        //        dato.Cantidad = row.Cells["cantidad"].Value.ToString();
-        //        dato.Precio = row.Cells["precio"].Value.ToString();
-        //        //agregar total de la boleta
-        //        double precioN = Convert.ToDouble(dato.Precio.ToString());
-        //        double cantidadN = Convert.ToDouble(dato.Cantidad.ToString());
-        //        montoTotal = montoTotal + (precioN * cantidadN);
-
-
-        //        if (row.Cells["numero"].Value.ToString() == vnum)
-        //        {
-        //            e.Graphics.DrawString(dato.Cantidad.ToString()
-        //            + "  |   " + dato.Nombre.ToString().Substring(0, dato.Nombre.Length > 30 ? 30 : dato.Nombre.Length), fuente, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //            e.Graphics.DrawString("|$" + dato.Precio.ToString(), fuente, Brushes.Black, new RectangleF(0, y += -5, ancho, 20), formato2);
-        //        }
-        //    }
-
-
-        //    e.Graphics.DrawString("                    ", font, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //    e.Graphics.DrawString("PRODUCTOS:", font, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //    e.Graphics.DrawString("—————————————————————", font, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //    e.Graphics.DrawString("Total:", font, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //    e.Graphics.DrawString(montoTotal.ToString(), font, Brushes.Black, new RectangleF(0, y += -5, ancho, 20), formato2);
-        //    e.Graphics.DrawString("                    ", font, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //    e.Graphics.DrawString("Pago con:", font, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //    e.Graphics.DrawString("Su cambio:", font, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //    e.Graphics.DrawString("                    ", font, Brushes.Black, new RectangleF(0, y += 20, ancho, 20));
-        //    e.Graphics.DrawString("GRACIAS POR SU VISITA", font, Brushes.Black, new RectangleF(25, y += 20, ancho, 20));
-        //    e.Graphics.DrawString("HASTA PRONTO", font, Brushes.Black, new RectangleF(70, y += 20, ancho, 20));
-
-        //}
+        
         public static StringBuilder line = new StringBuilder();
 
         private void btnSalir_Click(object sender, EventArgs e)
